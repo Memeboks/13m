80d6bed94912d79d1d6b0b0fe1ea9a24f6cba048
Memeboks <zlmark46@gmail.com>
2017-03-13 10:52:21 +0300

    Delete A.java

--- src/A.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class A {
-    public static void main (String [] args){
-        Scanner sc=new Scanner(System.in);
-        int q=sc.nextInt();
-        System.out.print(q+" "+(q*q)+" "+(q*q*q)+" "+(q*q*q*q));
-    }
-}
